92ef196838d29ee20801bfc8f3d9b946c18809a6
pvince@gmail.com <pvince@gmail.com@8e28dd6b-2a2c-43c4-bbda-1ddc0a028a48>
2010-02-04 17:34:12 +0000

    Created a communication command class.
    git-svn-id: svn+ssh://caffeine-server/media/storage/svn/SeniorDesign/trunk/Code@138 8e28dd6b-2a2c-43c4-bbda-1ddc0a028a48

---
+++ C#/CNCRouterCommand/CNCRouterCommand/CNCRCommCommand.cs
@@ -0,0 +1,22 @@
+﻿/**
+ * The majority of this section of the program was pulled from
+ * an article on www.dreamincode.com by PsychoCoder
+ * http://www.dreamincode.net/forums/showtopic35775.htm
+ * 
+ * It is distributed by PsychoCoder under the GNU General
+ * Public License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+
+namespace CNCRouterCommand
+{
+    class CNCRCommCommand
+    {
+        // TODO: Check out the SerialPortCommunication project in the C# code repository.
+    }
+}
