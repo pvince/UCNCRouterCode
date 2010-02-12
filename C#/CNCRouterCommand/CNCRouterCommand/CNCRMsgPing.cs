96238ca3a9edf91b625c5abb93b6b04b272fed8b
pvince@gmail.com <pvince@gmail.com@8e28dd6b-2a2c-43c4-bbda-1ddc0a028a48>
2010-02-12 04:20:16 +0000

    Added comments to CNCRMsgPing.cs
    git-svn-id: svn+ssh://caffeine-server/media/storage/svn/SeniorDesign/trunk/Code@181 8e28dd6b-2a2c-43c4-bbda-1ddc0a028a48

--- C#/CNCRouterCommand/CNCRouterCommand/CNCRMsgPing.cs
+++ C#/CNCRouterCommand/CNCRouterCommand/CNCRMsgPing.cs
@@ -5,11 +5,27 @@ using System.Text;
 
 namespace CNCRouterCommand
 {
+    /// <summary>
+    /// This message is sent by the computer to the router.  It is used to ensure
+    /// that the router is still connected.
+    /// 
+    /// Command Structure:
+    /// [Type 0000] [255]
+    /// -   Type: 0
+    /// </summary>
     public class CNCRMsgPing : CNCRMessage
     {
         public CNCRMsgPing() : base(CNCRMESSAGE_TYPE.PING)
         { }
 
+        /// <summary>
+        /// Transfers message data to a byte array for transfer.
+        /// </summary>
+        /// <returns>
+        ///  Command Structure:
+        ///     [Type ----] [255]
+        ///     [0000 0000] [255]
+        /// </returns>
         public override byte[] toSerial()
         { 
             // 0000 0000
