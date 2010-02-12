1f4578e9b7f3d4b372fb397190c62dc78f38e548
pvince@gmail.com <pvince@gmail.com@8e28dd6b-2a2c-43c4-bbda-1ddc0a028a48>
2010-02-12 04:30:48 +0000

    Added new way of getting the byte form of a CNCRMessage, need to refactor the CNCRMsg's to use this method.
    git-svn-id: svn+ssh://caffeine-server/media/storage/svn/SeniorDesign/trunk/Code@182 8e28dd6b-2a2c-43c4-bbda-1ddc0a028a48

--- C#/CNCRouterCommand/CNCRouterCommand/CNCRMessage.cs
+++ C#/CNCRouterCommand/CNCRouterCommand/CNCRMessage.cs
@@ -8,17 +8,26 @@ namespace CNCRouterCommand
     public abstract class CNCRMessage
     {
         private CNCRMESSAGE_TYPE _msgType;
+        private byte _msgTypeByte;
 
         public CNCRMessage(CNCRMESSAGE_TYPE msgType)
         {
             _msgType = msgType;
+            _msgTypeByte = Convert.ToByte(Convert.ToByte(_msgType) << 4);
         }
 
         public abstract byte[] toSerial();
 
+        //TODO: Change MessageType to getMessageType()
         public CNCRMESSAGE_TYPE MessageType
         {
             get { return _msgType; }
+        }
+
+        //TODO: Make messages use this instead of doing the conversion every time.
+        public byte getMsgTypeByte()
+        {
+            return _msgTypeByte;
         }
     }
 }
